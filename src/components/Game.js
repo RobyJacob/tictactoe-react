c1c758263cefa48a30850764125e174f2341728e
roby j <roby@robys-MacBook-Pro.local>
2023-03-18 12:46:12 +0530

    update game-info for current move with text

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,14 +18,20 @@ function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) description = 'Go to move #' + move
-    else description = 'Go to game start'
+    if (move > 0) {
+        if (move === curMove) description = 'You are at move #' + move
+        else description = 'Go to move #' + move
+    } else {
+        description = 'Go to game start'
+    }
 
     return (
       <li>
-        <button onClick={ () => jumpTo(move) }>
-          { description }
-        </button>
+      {
+        move === curMove ?
+        <p> { description } </p> : 
+        <button onClick={ () => jumpTo(move) }> { description } </button>
+      }
       </li>
     )
   })
