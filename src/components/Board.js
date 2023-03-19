21bd5bbdd272852bc53cac716f9a56eecef7d35d
roby j <roby@robys-MacBook-Pro.local>
2023-03-19 13:04:38 +0530

    remove redundant code for board generation

--- src/components/Board.js
+++ src/components/Board.js
@@ -32,27 +32,31 @@ function Board({ xIsNext, squares, onPlay }) {
     }
   
     const status = winner ? 'Winner: ' + winner : 'Next player: ' + (xIsNext ? 'X' : 'O')
-  
+    
+    const setUp = () => {
+        const board = []
+        const squareComps = []
+        
+        for (let i = 0; i < 3; i++) {
+            const squareComp = []
+            for (let j = 0; j < 3; j++) {
+                const idx = 3 * i + j
+                squareComp.push(<Square value={ squares[idx] } clickHandler={ () => handleClick(idx) } />)
+            }
+            squareComps.push(squareComp)
+        }
+
+        for (let i = 0; i < 3; i++) {
+            board.push(<div className='board-row'> { squareComps[i] } </div>)
+        }
+
+        return board
+    }
+
     return (
       <>
         <div className='status'> { status } </div>
-        <div className='board-row'>
-          <Square value={ squares[0] } clickHandler={ () => handleClick(0) }/>
-          <Square value={ squares[1] } clickHandler={ () => handleClick(1) }/>
-          <Square value={ squares[2] } clickHandler={ () => handleClick(2) }/>
-        </div>
-  
-        <div className='board-row'>
-          <Square value={ squares[3] } clickHandler={ () => handleClick(3) }/>
-          <Square value={ squares[4] } clickHandler={ () => handleClick(4) }/>
-          <Square value={ squares[5] } clickHandler={ () => handleClick(5) }/>
-        </div>
-  
-        <div className='board-row'>
-          <Square value={ squares[6] } clickHandler={ () => handleClick(6) }/>
-          <Square value={ squares[7] } clickHandler={ () => handleClick(7) }/>
-          <Square value={ squares[8] } clickHandler={ () => handleClick(8) }/>
-        </div>
+        { setUp() }
       </>
     )
 }
